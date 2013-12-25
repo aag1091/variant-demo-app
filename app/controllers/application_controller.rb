a84f3399b7539125c139437fb9cbfc1dc5f9c199
Avinash Gosavi <aag1091@gmail.com>
2013-12-25 16:24:17 +0530

    configured mobivious and added pages for mobile and tablet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :detect_device_variant
+
+  private
+
+  def detect_device_variant
+    case request.env['mobvious.device_type']
+    when :desktop
+      request.variant = :desktop
+    when :tablet
+      request.variant = :tablet
+    when :mobile
+      request.variant = :phone
+    end
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
   def index
     @comics = Comic.all
+    respond_to do |format|
+      format.json
+      format.html               # /app/views/posts/show.html.erb
+      format.html.phone         # /app/views/posts/show.html+phone.erb
+      format.html.tablet        # /app/views/posts/show.html+tablet.erb
+    end
+    # we can also define the render method as below
+    # respond_to do |format|
+    #   format.html do |variant|
+    #     variant.tablet
+    #     variant.phone
+    #     variant.desktop
+    #     variant.none
+    #   end
+    # end
   end
 end
